b2737b740e0a3e24531f53696f823303d31d0fa0
Ranjit Kairi <ranjitkoiri009@gmail.com>
2024-08-12 00:27:21 +0530

    feat(): fix all the flow 4 bugs

--- components/header/header.js
+++ components/header/header.js
@@ -4,6 +4,22 @@ const ctaNav = document.querySelector('.cta-nav');
 const nav = document.querySelector('nav');
 const dropdownItems = document.querySelectorAll('.nav-links li .dropdown');
 
+// Get the current pathname
+const currentPath = window.location.pathname;
+const navLinks = document.querySelectorAll('nav ul.nav-links > li > a');
+
+// Remove 'active-nav' from all links
+navLinks.forEach(link => {
+  link.classList.remove('active-nav');
+});
+
+// Add 'active-nav' to the link that matches the current pathname
+navLinks.forEach(link => {
+  if (link.getAttribute('href') === currentPath) {
+    link.classList.add('active-nav');
+  }
+});
+
 function toggleNavHeaderButtons() {
   if (ctaNav.classList.contains('active') || nav.classList.contains('active')) {
     menuToggle.style.display = 'none';
@@ -24,9 +40,4 @@ closeButton.addEventListener('click', () => {
   ctaNav.classList.remove('active');
   nav.classList.remove('active');
   toggleNavHeaderButtons();
-});
-
-dropdownItems.forEach((dropdown) => {
-  dropdown.addEventListener('mouseenter', toggleNavHeaderButtons);
-  dropdown.addEventListener('mouseleave', toggleNavHeaderButtons);
 });
